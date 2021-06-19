76bb65466d61ae9bf61bcd8cca39af67ad28d0f7
vitor rodrigues <vitorsilva.com@gmail.com>
2021-06-19 19:12:33 +0100

    load file and send to console log

--- from_latlong_to_streetname.js
+++ from_latlong_to_streetname.js
@@ -1,18 +1,18 @@
 /*
-    get file list
+    get file list -- https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
     open file (json)
     get required elements
     push into array
     save array into csv
 */
 
-//https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
-
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
+
 //joining path of directory 
 const directoryPath = path.join(__dirname, '..\\json');
+
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
     //handling error
@@ -23,5 +23,22 @@ fs.readdir(directoryPath, function (err, files) {
     files.forEach(function (file) {
         // Do whatever you want to do with the file
         console.log(file); 
+
+        let rawdata = fs.readFileSync('D:\\ONE DRIVE - PESSOAL\\OneDrive\\Site_Doutoramento\\LATLONG_STREET\\json\\' + file);
+        let address = JSON.parse(rawdata);
+
+        if (!address.error) {
+            console.log(address.features[0].properties.address_line1); 
+            console.log(address.features[0].properties.address_line2); 
+            console.log(address.features[0].properties.formatted); 
+            console.log(address.features[0].properties.street);     
+            console.log(address.features[0].properties.housenumber);     
+        }
+
     });
 });
+
+//address.features[0].properties.address_line1
+//address.features[0].properties.address_line2
+//address.features[0].properties.formatted
+//address.features[0].properties.street
