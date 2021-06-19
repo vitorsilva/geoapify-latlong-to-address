f4ed18ea4c22a3f343649c3c906443e5b5dca140
vitor rodrigues <vitorsilva.com@gmail.com>
2021-06-19 18:56:38 +0100

    get file list

--- from_latlong_to_streetname.js
+++ from_latlong_to_streetname.js
@@ -5,3 +5,23 @@
     push into array
     save array into csv
 */
+
+//https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
+
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, '..\\json');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
