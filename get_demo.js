1b42eeb4e317796fb4e52df74ae3f04d6be9a3ab
vitor rodrigues <vitorsilva.com@gmail.com>
2021-06-19 16:37:50 +0100

    first call to geoapify

--- get_demo.js
+++ get_demo.js
@@ -1,7 +1,10 @@
 //https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
 const https = require('https');
+require("dotenv").config();
 
-https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+const url = "https://api.geoapify.com/v1/geocode/reverse?lat=41.15051389&lon=-8.611091667&api_key=" + process.env.GEOAPIFY_API_KEY;
+
+https.get(url, (resp) => {
   let data = '';
 
   // A chunk of data has been received.
@@ -11,7 +14,8 @@ https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
+    //console.log(JSON.parse(data).explanation);
+    console.log(data);
   });
 
 }).on("error", (err) => {
